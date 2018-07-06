e537e82be71f4057648011c9bf39a12fcf5c577b
mweiss17 <martin.weiss@mail.mcgill.ca>
2018-07-06 18:32:13 -0400

    bumping version number

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('LICENSE') as f:
 
 setup(
     name='academictorrents',
-    version='2.0.7',
+    version='2.0.8',
     description='Academic Torrents Python APIs',
     long_description=readme,
     author='Martin Weiss, Alexis Gallepe, Jonathan Nogueira',
