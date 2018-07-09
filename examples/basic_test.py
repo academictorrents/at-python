6fb051910650cd83de928e9c9d50f99013b492b9
mweiss17 <martin.weiss@mail.mcgill.ca>
2018-07-09 15:02:48 +0200

    updating example basic test to python 3 compatibility

--- examples/basic_test.py
+++ examples/basic_test.py
@@ -1,22 +1,18 @@
 
-print "About to import the library"
 
 import academictorrents as at
+import cPickle
+import gzip
+import sys
+import os
+import time
+import numpy as np
 
-
-
-
-print "About to start a download"
+print("Libraries imported, about to start a download")
 
 filename = at.get("323a0048d87ca79b68f12a6350a57776b6a3b7fb")
 
-
-
-print "About to open the file"
-
-import cPickle, gzip
-import sys, os, time
-import numpy as np
+print("About to open the file")
 
 mnist = gzip.open(filename, 'rb')
 train_set, validation_set, test_set = cPickle.load(mnist)
