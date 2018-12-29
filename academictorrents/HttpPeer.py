ebfd1ccc9656ef7ee0a8ce6fe89231d914eaf425
mweiss17 <martin.weiss@mail.mcgill.ca>
2018-12-28 19:18:56 -0500

    tweaking params

--- academictorrents/HttpPeer.py
+++ academictorrents/HttpPeer.py
@@ -38,7 +38,7 @@ class HttpPeer(object):
         size = 0
         temp_size = 0
         max_size_to_download = 5000000
-        max_num_pieces = 5
+        max_num_pieces = 50
         temp_pieces = []
         pieces = []
 
