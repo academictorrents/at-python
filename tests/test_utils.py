b01ab32ccc0bba679d8904f7ee4f629056b9b9d9
Martin Weiss <martin.clyde.weiss@gmail.com>
2019-01-06 18:47:54 -0500

    removed breakpoint

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -71,6 +71,5 @@ class UtilsTestSuite(unittest.TestCase):
     # Test with different datastore
     def test_different_datastore(self):
         filename = at.get('323a0048d87ca79b68f12a6350a57776b6a3b7fb', datastore='~/.academictorrent-datastore/alt/')
-        import pdb; pdb.set_trace()
         assert filename == os.path.expanduser('~/.academictorrent-datastore/alt/mnist.pkl.gz')
         self.assertTrue(os.path.isfile(filename))
