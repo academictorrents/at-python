98f5d36865ddab86f2e1745f6a9fe8cdd4ba628b
Pierre Tholoniat <pierre@tholoniat.com>
2021-05-01 15:57:18 -0400

    Added a simple command line interface

--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
 from setuptools import setup, find_packages
-exec(open('academictorrents/version.py').read())
 
-with open('README.md') as f:
+exec(open("academictorrents/version.py").read())
+
+with open("README.md") as f:
     readme = f.read()
 
-with open('LICENSE') as f:
+with open("LICENSE") as f:
     license = f.read()
 
 setup(
-    name='academictorrents',
+    name="academictorrents",
     version=__version__,
-    description='Academic Torrents Python API',
-    author='Martin Weiss, Alexis Gallepe, Jonathan Nogueira',
-    author_email='contact@academictorrents.com',
-    data_files = [("", ["LICENSE"])],
-    url='https://github.com/AcademicTorrents/python-r-api',
+    description="Academic Torrents Python API",
+    author="Martin Weiss, Alexis Gallepe, Jonathan Nogueira",
+    author_email="contact@academictorrents.com",
+    data_files=[("", ["LICENSE"])],
+    url="https://github.com/AcademicTorrents/python-r-api",
     classifiers=(
-        "Programming Language :: Python :: 2.6"
-        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 2.6" "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.0",
         "Programming Language :: Python :: 3.1",
@@ -28,13 +28,16 @@ setup(
         "Programming Language :: Python :: 3.6",
     ),
     license=license,
-    packages=find_packages(exclude=('tests', 'docs')),
+    packages=find_packages(exclude=("tests", "docs")),
     install_requires=[
-        'bencode.py==2.0.0',
-        'bitstring==3.1.5',
-        'PyPubSub==3.3.0',
-        'requests>=2.19.0',
-        'future==0.16.0',
-        'tqdm',
+        "bencode.py==2.0.0",
+        "bitstring==3.1.5",
+        "PyPubSub==3.3.0",
+        "requests>=2.19.0",
+        "future==0.16.0",
+        "tqdm",
     ],
+    entry_points={
+        "console_scripts": ["atget = academictorrents:cli"],
+    },
 )
